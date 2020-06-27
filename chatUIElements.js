06fbe12776086fadb0b85ce02ca2455f9096f3f2
Dima_S <dimapepsi@mail.ru>
2020-06-27 13:53:43 +0300

    Правки по чату

--- chatUIElements.js
+++ chatUIElements.js
@@ -1,2 +1,5 @@
-export const el = document.querySelector(".button-tuning");
-export const em = document.querySelector(".menu ");
+export const btnTuning = document.querySelector(".header__tuning");
+export const btnHeadExit = document.querySelector(".header__exit");
+export const areaDialog = document.querySelector(".main");
+export const areaMessage = document.querySelector(".message");
+export const btnExit = document.querySelector(".exit");
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-console.log("Привет, я модуль main.js");
 import {serverURL} from './config.js';
 console.log(`${serverURL} это я принял из config.js`);
-import {el} from './chatUIElements.js';
-import {em} from './chatUIElements.js';
-console.log(`${el} это я принял из chatUIElements.js`);
-console.log(`${em} это тоже из chatUIElements.js`);
+import {btnTuning} from './chatUIElements.js';
+import {btnHeadExit} from './chatUIElements.js';
+import {areaDialog} from './chatUIElements.js';
+import {areaMessage} from './chatUIElements.js';
+import {btnExit} from './chatUIElements.js';
+
